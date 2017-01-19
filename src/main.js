285ad2c3d20ac9cbf5462183940c340f36412427
Ben Foxall <benfoxall@gmail.com>
2017-01-19 15:55:09 +0000

    Load template and hook up to Hexie ui in panel

--- src/main.js
+++ src/main.js
@@ -1,21 +1,71 @@
-const h1 = document.createElement('h1')
+import Template from './template.html'
+import hexie from './hexie-ui.js'
 
-h1.innerText = 'this does not work yet'
-
-Object.assign(
-  h1.style,
-  {
-    background: 'aquamarine',
-    color: '#000',
-    fontFamily: 'sans-serif',
-    fontSize: '2vw',
-    padding: '1em',
-    position: 'fixed',
-    margin: 0,
-    top: 0,
-    right: 0,
-    zIndex: 9999
+const element = new Template({
+  target: document.body,
+  data: {
+    eval: '',
+    expanded: false
   }
-)
+})
 
-document.body.appendChild(h1)
+const panel = element.refs.panel
+
+element.observe( 'eval', answer => {
+  let target
+
+  element.set({expanded: false})
+
+  if(!answer) {
+    return panel.innerText = ''
+  }
+
+  try {
+    target = eval(answer)
+  } catch (e) {
+    panel.innerText = `${answer} not found`
+  }
+
+  if(typeof target == 'undefined') {
+    panel.innerText = `${answer} is undefined `
+  }
+  else {
+
+    // urgh
+    [
+      Int8Array, Uint8Array, Uint8ClampedArray, Int16Array, Uint16Array, Int32Array, Uint32Array, Float32Array, Float64Array
+    ].forEach(type => {
+      if(target instanceof type) {
+        target = target.buffer
+      }
+    })
+
+
+    if(!(target instanceof ArrayBuffer)) {
+      panel.innerText = `${answer} is not an ArrayBuffer or typed array`
+    } else {
+      panel.innerHTML = ''
+
+      const cols = 16
+      const rows = Math.min(750,target.byteLength / cols)
+
+
+
+      const ui = hexie(rows, cols, panel)
+
+      const view = new DataView(target)
+
+      ui(view)
+
+      element.set({expanded: true})
+
+    }
+
+  }
+
+})
+
+
+window.hexspector = e => {
+  element.set({eval: e})
+}
